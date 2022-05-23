44e66c3d47c34a2ebeb62fed2fa349948bcfe5c0
Kreamsoup <alexalex225225@gmail.com>
2022-05-24 03:14:06 +0900

    20220524.0311 prodotype2.py : delete overlapped code(class DLinkedList())

--- prototype2.py
+++ prototype2.py
@@ -18,41 +18,6 @@ class StationNode:
         # print("hahahahahah")
         
 
-
-class DLinkedList():
-    def __init__(self, station,transf_to=None):
-        self.head = StationNode(station)
-        self.tail = self.head
-        if(transf_to!=None):
-            self.next=transf_to     # transf_to = line number
-
-    def push(self, station):
-        if self.head == None:
-            self.head = StationNode(station)
-            self.tail = self.head
-        else:
-            current = self.head
-            while current.next != None:
-                current = current.next
-            insertnode = StationNode(station,prev=current)
-            current.next = insertnode
-            self.tail = insertnode
-    
-    def pop(self):
-        current = self.head
-        last = self.head.next
-        while last.next != None:
-            current = current.next
-            last = last.next
-        last=None
-        current.next=None
-
-    def printlist(self):
-        current = self.head
-        while current != None:
-            print("{}".format(current.data))
-            current = current.next
-
 StationList=[]
 
 def search_station(searchname, line=None):
