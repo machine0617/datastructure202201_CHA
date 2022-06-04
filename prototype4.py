36db0feefc590cb6403a91a2200aec698b1a5f81
machine0617 <105858176+machine0617@users.noreply.github.com>
2022-06-04 09:43:54 +0900

    fixed some error

--- prototype4.py
+++ prototype4.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-#파일 전처리(이상한 부분 삭제)
-filename = './edges.csv'
-rawData = pd.read_csv(filename,encoding='UTF-8')
-file =  rawData
+filename = 'C:/Users/SAMSUNG/source/edges.csv'
+file = pd.read_csv(filename,encoding='UTF-8') 
 
 class StationNode:
     def __init__(self, name, line):
@@ -30,7 +28,7 @@ class StationNode:
             print("\"{}({}) : {}\"".format(trsf1.name,trsf1.line, trsf[1]),end='')
         print("]")
 
-def append_next(self):
+def append_next(self): 
     self.next.append([])
     return len(self.next)-1
 
@@ -45,21 +43,21 @@ def search_station(searchname, searchline=None):
     if searchline!=None and searchline>0 and searchline<=len(NodeList):
         for _, Stations in enumerate(NodeList):
             for j, station in enumerate(Stations):
-                # station.printnode()
-                # print("searchname{},{}/".format(type(searchname),searchname))
-                # print("stationame{},{}/".format(type(station.name),station.name))
-                # print("searchline{},{}/".format(type(searchline),searchline))
-                # print("stationlin{},{}/".format(type(station.line),station.line))
-                # print(station.name == searchname)
-                # print(station.line == searchline)
+                #station.printnode()
+                #print("searchname{},{}/".format(type(searchname),searchname))
+                #print("stationame{},{}/".format(type(station.name),station.name))
+                #print("searchline{},{}/".format(type(searchline),searchline))
+                #print("stationlin{},{}/".format(type(station.line),station.line))
+                #print(station.name == searchname)
+                #print(station.line == searchline)
                 if (station.name == searchname) and (station.line == searchline):
                     # print("!!!!!!!!!!")
                     return station
     else:  #찾고자 하는 특정 호선이 존재 안 함.
         # print("NodeList={}".format(NodeList))
         for i, Stations in enumerate(NodeList):
-            # print("i={}, Stations=  {}".format(i, Stations))
-            # print("search from line#{}".format(i+1))
+            #print("i={}, Stations=  {}".format(i, Stations))
+            #print("search from line#{}".format(i+1))
             for j, station in enumerate(Stations):
                 if station.name == searchname:
                     return station
@@ -71,9 +69,10 @@ def search_station(searchname, searchline=None):
 def getlinelist():
     #Data로 file 사용
     for (idx, row) in (file.iterrows()):
-        time = row[2]
-        stationname = row[1]
-        stationline = row[0]
+        idx = row[0]
+        stationline = row[1]
+        stationname = row[2]
+        time = row[3]
         # print("rdline:\"{}\"".format(rdline))
         #print("{}".format(stationname))
         # print("stations={}".format(stations))
@@ -90,7 +89,7 @@ def getlinelist():
 
                 # 만약 새로운역이면 추가. 이미 존재하는역(현재 라인 내에서 한정)일경우 패스
         if found == False:  # new station in current line
-            if(stationline != file.iloc[idx-1,0]):       #시작점 (호선이 달라지는 경우)
+            if(stationline != file.iloc[idx-1,1]):       #시작점 (호선이 달라지는 경우)
                 newnode = StationNode(stationname, stationline)
                 ##환승역 체크. 다른노선에 동일명의 역이 존재할경우 trsf에 추가##
                 trsffound = search_station(stationname)
@@ -104,16 +103,16 @@ def getlinelist():
                     trsffound.trsf[j].append(edge)
                     #print(newnode.trsf)
                     #print(trsffound.trsf)
-           ###################################################
-                print(newnode.name, newnode.next, newnode.trsf)
-                NodeList[stationline-1].append(newnode)
+            ##################################################
+                #NodeList[stationline-1].append(newnode)
 
             else:      # 그냥 중간역
                 newnode = StationNode(stationname, stationline)
                 ##환승역 체크. 다른노선에 동일명의 역이 존재할경우 trsf에 추가##
-                for checkline in range(1,5):
+                for checkline in range(1,stationline):
                     trsffound = search_station(stationname,checkline)
                     if trsffound != False:
+                        #print("{}/{} in {}".format(trsffound.name, trsffound.line, checkline))
                         edge = 3
                         i = append_trsf(newnode)
                         j = append_trsf(trsffound)
@@ -124,8 +123,8 @@ def getlinelist():
                         #print(newnode.trsf)
                         #print(trsffound.trsf)
                         ###################################################
-
-                prevstation = search_station(file.iloc[idx-1,1],file.iloc[idx-1,0])
+                prevstation = search_station(file.iloc[idx-1,2],file.iloc[idx-1,1])
+                #print(prevstation)
                 i = append_next(prevstation)
                 prevstation.next[i].append(newnode)
                 newnode.prev.append(prevstation)
@@ -133,7 +132,7 @@ def getlinelist():
                 newnode.prev.append(time)
                 prev = newnode.prev[0]
 
-                NodeList[stationline-1].append(newnode)
+        NodeList[stationline-1].append(newnode)
        
         # print("-------------------------------------------")
         # for i,NodeLine in enumerate(NodeList):
@@ -146,11 +145,17 @@ def getlinelist():
 
 NodeList = []
 getlinelist()
-#sicheong = search_station("시청",2)
-#chungjeongro = search_station("충정로",2)
-#sicheong.prev.append(chungjeongro)
-#chungjeongro.next.append(sicheong)
 
+
+##2호선 순환
+
+ttukseom = search_station("뚝섬",2)
+seongsu = search_station("성수",2)
+ttukseom.prev.append(seongsu)
+ttukseom.prev.append(1)
+seongsu.next.append([])
+seongsu.next[0].append(ttukseom)
+seongsu.next[0].append(1)
 
 ############ search_station ############
 ## let's try testing search function! change "을지로" to another station name.
@@ -161,4 +166,3 @@ getlinelist()
 for i,NodeLine in enumerate(NodeList):
     for j, Node in enumerate(NodeLine):
         Node.printnode()
-
