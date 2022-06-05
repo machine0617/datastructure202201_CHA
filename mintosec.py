8be78020f583d3d7503c86d3567d98dec2488a7f
송서현 <songseohyeon@songseohyeons-MacBook-Air.local>
2022-06-06 04:43:22 +0900

    min to sec

---
+++ mintosec.py
@@ -0,0 +1,14 @@
+import pandas as pd
+filename = './edges_ori.csv'
+file = pd.read_csv(filename,encoding='UTF-8') 
+
+output = open("output.csv",'w',encoding='utf-8')
+output.write("호선,역,시간(sec)\n")
+for (idx, row) in (file.iterrows()):
+    if type(row[2])==str:
+        time = row[2].split(':')
+        sec = int(time[0])*60 + int(time[1])
+        print(sec)
+        output.write("{},{},{}\n".format(row[0],row[1],sec))
+    else:
+        output.write("{},{},\n".format(row[0],row[1]))
